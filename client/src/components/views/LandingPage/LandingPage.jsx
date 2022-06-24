05e31b27270c4032bf32114872c691be88b8b0ed
wisdom <wisdom08@kakao.com>
2022-06-24 15:00:24 +0900

    feat: 로그아웃 구현

--- client/src/components/views/LandingPage/LandingPage.jsx
+++ client/src/components/views/LandingPage/LandingPage.jsx
@@ -1,11 +1,27 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
+ import {useNavigate} from "react-router-dom";
 
 const LandingPage = () => {
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get('http://localhost:3001/').then(response => console.log(response));
     })
+
+
+    const onClickHandler = () => {
+        axios.get('http://localhost:3001/api/users/logout')
+            .then(response => {
+                if (response.data.success) {
+                    navigate("/login")
+                } else {
+                    alert('로그아웃 하는데 실패 했습니다.')
+                }
+            })
+    }
+
 
     return (
         <div style={{
@@ -13,6 +29,9 @@ const LandingPage = () => {
             width:'100%', height:'100vh'
         }}>
             <h2>시작 페이지</h2>
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 };
